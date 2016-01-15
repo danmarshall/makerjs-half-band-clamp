a0e47d85f016121deaf2f4be8202219d0791c21c
Dan Marshall <danmar@microsoft.com>
2016-01-15 07:37:58 -0800

    added test

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-console.log('success');
+var makerjs = require('makerjs');
+var HalfBandClamp = require('./index');
+var h = new HalfBandClamp(12.7, 3, 12, 5);
+
+console.log(makerjs.exporter.toDXF(h));
